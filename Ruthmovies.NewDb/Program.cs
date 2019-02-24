7ba94ed4601b5d84711b775e1e46e41a90df4ad6
ruth07 <mukamutararuth1@gmail.com>
2019-02-24 06:48:23 -0800

    movies work

---
+++ Ruthmovies.NewDb/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MovieApp.NewDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new MovieContext())
+            {
+                Console.WriteLine("enter Movie title");
+                string title = Console.ReadLine();
+                Console.WriteLine("enter Movie release year");
+                string year = Console.ReadLine();
+                Console.WriteLine("enter Movie category");
+                string categories = Console.ReadLine();
+                db.Movies.Add(new Movietbl { title = title });
+                db.Movies.Add(new Movietbl { year = year });
+                db.Movies.Add(new Movietbl { category_name = categories });
+                var count = db.SaveChanges();
+                Console.WriteLine("-{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("title:" + "Year" + "Category");
+                Console.WriteLine("---------------------------------------------------------");
+                foreach (var Movietbl in db.Movies)
+                {
+                   
+                    Console.Write("{0}, {1}, {2}",Movietbl.title, Movietbl.year,Movietbl.category_name,"\r \r \r");
+                    Console.WriteLine("");
+                   
+                } 
+            }
+        }
+    }
+}
